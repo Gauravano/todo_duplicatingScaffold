fc8440a5064f9615d5c1a071e1a1c612255ba6ef
gauravano <sachdeva.gaurav1997@gmail.com>
2017-06-29 16:31:57 +0530

    todo controller completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,60 @@
 class TodosController < ApplicationController
+
+  before_action :set_todo,only: [:show,:edit,:update,:destroy]
+  before_action :print_this  
+
   def new
+    @todo = Todo.new
   end
 
   def edit
   end
 
   def create
+    todo = params["todo"]
+    todo = todo.permit(:title,:content,:urgent,:important,:deadline)
+    @todo = Todo.new(todo)
+    is_save = @todo.save
+
+    unless is_save
+      render :new
+    else
+      redirect_to '/todos/'
+
   end
 
   def destroy
+    @todo.destroy
+    redirect_to '/todos/'
   end
 
   def update
+    todo = params["todo"]
+    todo = todo.permit(:title,:content,:urgent,:important,:deadline)
+    is_update = @todo.update
+
+    unless is_update
+      render :edit
+    else
+      redirect_to '/todos/'
+
   end
 
   def index
+    @todos = Todo.all
   end
 
   def show
   end
+
+private
+  def set_todo
+    puts "Setting Todo"
+    @todo = Todo.find(params["id"])
+  end
+
+def print_this
+  puts "Testing of beforeAction"
+end
+
 end
